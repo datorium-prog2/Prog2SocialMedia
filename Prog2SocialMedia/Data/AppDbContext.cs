d00b3a96697f526febbfef91a2aadc60bfdb32be
rebeka.pilscikova <rebeka.pilscikova@visma.com>
2026-01-13 13:04:19 +0200

    add db context

---
+++ Prog2SocialMedia/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Prog2SocialMedia.Models;
+
+namespace Prog2SocialMedia.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        }
+
+        // Norādam kādas tabulas būs mūsu datubāzē
+        public DbSet<Post> Posts => Set<Post>();
+    }
+}
