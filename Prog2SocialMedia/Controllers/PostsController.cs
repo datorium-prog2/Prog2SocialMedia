357c3686989cb91048eb8c425ed6e63c755ec4e9
rebeka.pilscikova <rebeka.pilscikova@visma.com>
2026-01-13 12:24:11 +0200

    add posts

---
+++ Prog2SocialMedia/Controllers/PostsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Prog2SocialMedia.Controllers
+{
+    [ApiController]
+    [Route("api/posts")]
+    public class PostsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPosts()
+        {
+            return Ok("POSTS");
+        }
+    }
+}
