f83dc813570f7f0834da0ed18d9c8821f6c0be71
rebeka.pilscikova <rebeka.pilscikova@visma.com>
2026-01-13 12:39:02 +0200

    add post model

---
+++ Prog2SocialMedia/Models/Posts.cs
@@ -0,0 +1,10 @@
+﻿namespace Prog2SocialMedia.Models
+{
+    public class Posts
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateAt { get; set; }
+    }
+}
